44df4d69ea9b8ce71d84d514eddbb1ecfb00853b
Teresa Chaves <ttahaves@hotmail.com>
2022-12-09 11:56:18 +0100

    aftercodereview

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -23,41 +23,35 @@ router.get("/serviceDetails/:service_id", (req, res, next) => {
 
 
 router.post("/addService", isAuthenticated, (req, res, next) => {
+
     const { name, description, image, date, status } = req.body
+    const { _id: owner } = req.payload
+
     Service
-        .create({ name, description, image, date, status, owner: req.payload._id })
+        .create({ name, description, image, date, status, owner })
         .then(response => res.json(response))
-
-
         .catch(err => next(err))
 })
 
 router.put("/edit-service/:service_id", (req, res, next) => {
 
-    const { service_id: id } = req.params
+    const { service_id } = req.params
     const { name, description, image } = req.body
 
     Service
-        .findByIdAndUpdate(id, { name, description, image }, { new: true })
+        .findByIdAndUpdate(service_id, { name, description, image }, { new: true })
         .then(response => res.json(response))
         .catch(error => { next(error) })
 })
 
-router.delete('/delete-service/:service_id', (req, res, next) => {
+router.delete('/delete-service/:service_id', isAuthenticated, (req, res, next) => {
 
-    const { service_id: id } = req.params
+    const { service_id } = req.params
 
     Service
-        .findByIdAndDelete(id)
+        .findByIdAndDelete(service_id)
         .then(response => res.json(response))
-        .then(service => {
-            res.redirect("./servicios", {
-                service,
-                isADMIN: req.session.currentUser.role === 'ADMIN'
-            })
-        })
         .catch(error => { next(error) })
-
 })
 
 
