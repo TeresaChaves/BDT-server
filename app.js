02f75c9aa0462096ca1d5947e2824b38b3fdb462
TeresaChaves <113774017+TeresaChaves@users.noreply.github.com>
2025-05-29 10:52:50 +0200

    connectSrc

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:", "https:"],
+      connectSrc: ["'self'", "https://timeshare.fly.dev"],
     },
   })
 );
